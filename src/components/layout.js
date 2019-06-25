f84053701b6703061d8f609bc44ce2dc6a3cb05f
pixtop <pixtopmc@gmail.com>
2019-06-25 09:48:11 +0100

    refactor layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,23 @@
 import './layout.css'
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 
 import SocialBar from './SocialBar/SocialBar';
-import TopBar from './TopBar/TopBar';
+import Header from './header';
+import Footer from './footer';
 
 
-const Layout = ({ banner, children }) => {
-
-  let mainStyle = {}
-  if(banner){
-    mainStyle = {
-      backgroundImage: 'url("../' + banner + '")',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'top',
-      backgroundSize: 'auto',
-      paddingTop: '300px'
-    }
-  }
-
-  return (
-    <StaticQuery
-      query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-      render={data => (
-        <React.Fragment>
-          <TopBar />
-          <SocialBar />
-          <main style={mainStyle} >{children}</main>
-        </React.Fragment>
-      )}
-    />
-  )
-}
+const Layout = ({ banner, children }) => (
+  // shortcut to React.Fragment
+  <>
+    <Header />
+    <SocialBar />
+    <main>
+      {children}
+    </main>
+    <Footer />
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
