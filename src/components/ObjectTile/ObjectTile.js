148e4640778f775a928c3344bd5ca2745b35df76
pixtop <pixtopmc@gmail.com>
2019-06-26 14:53:21 +0100

    beautify object tile and optimize source code

--- src/components/ObjectTile/ObjectTile.js
+++ src/components/ObjectTile/ObjectTile.js
@@ -1,30 +1,19 @@
-
 import './ObjectTile.scss'
 
 import React from 'react';
 
-class ObjectTile extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            object: props.object ? props.object : {},
-            isLoaded: false
-        };
-    }
-
-    render() {
-        return (
-            <a className="object-tile-a" href={this.state.object.url} >
-                <div className="object-tile" style={{backgroundImage: `url(${this.state.object.images[0].thumbnail.url})`}}>
-                    <div className="info">
-                        <p>{this.state.object.name}</p>
-                    </div>
-                </div>
-            </a>
-        );
-    }
-
+function ObjectTile({ object }) {
+  return (
+      <a className="object-tile-a" href={object.url} >
+        <div className="object-tile" style={{
+          backgroundImage: `url(${object.images[0].thumbnail.url})`,
+        }}>
+          <div className="info">
+            <p>{object.name}</p>
+          </div>
+        </div>
+      </a>
+  );
 }
 
 export default ObjectTile;
