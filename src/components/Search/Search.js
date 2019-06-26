41c40ae72fae46c023dc71bcb4940e4739d87b3d
pixtop <pixtopmc@gmail.com>
2019-06-26 14:52:33 +0100

    beautify search aspect and optimize source code

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -2,32 +2,33 @@ import './Search.scss'
 
 import React from 'react';
 import Autosuggest from 'react-autosuggest';
+import {FaSearch} from 'react-icons/fa';
 import config from '../../config';
 
 const adaptSuggestions = value => {
-    return value.slice(0, 4).map(item =>
-      ({ 
-          name: item[1],
-          image: item[2],
-          designer: item[3],
-          url: item[4]
-      })
-  );
+  return value.slice(0, 4).map(item => ({name: item[1], image: item[2], designer: item[3], url: item[4]}));
 };
 
 const getSuggestionValue = suggestion => suggestion.name;
 
-const renderSuggestion = suggestion => (
-  <div className="search-suggestion" >
-    <a href={suggestion.url}>
-    <img className="image" alt="suggested object" src={suggestion.image}/>
-    <div className="text" title={suggestion.name}>
-        {suggestion.name}<br/>
-        <span className="designer-name">{suggestion.designer}</span>
-    </div>
-    </a>
-  </div>
-);
+function renderSuggestion(suggestion) {
+  return (
+    <a className="url-suggestion" href={suggestion.url}>
+      <div className="search-suggestion">
+        <img alt="suggested object" src={suggestion.image}/>
+        <div className="text" title={suggestion.name}>
+          <p>{suggestion.name}</p>
+          <p className="author">{suggestion.designer}</p>
+        </div>
+      </div>
+    </a>);
+}
+
+const renderInput = inputProps => (
+  <form>
+    <label><FaSearch/></label>
+    <input {... inputProps}/>
+  </form>);
 
 class Search extends React.Component {
   constructor(props) {
@@ -45,53 +46,33 @@ class Search extends React.Component {
     this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this)
   }
 
-    onChange = (event, { newValue }) => {
-        this.setState({
-            value: newValue
-        });
-    };
+  onChange = (event, {newValue}) => {
+    this.setState({value: newValue});
+  };
 
   changeSorting(sortBy) {
-    this.setState({
-      sortBy: sortBy
-    });
+    this.setState({sortBy: sortBy});
     this.props.onSearch(this.state.value, sortBy)
   }
 
-    keyPressed = (event) => {
-        if (event.key === "Enter") {
-          this.setState({
-            sortBy: 'popular' 
-          });
-          this.props.onSearch(this.state.value, 'popular')
-        }
-    };
+  keyPressed = (event) => {
+    if (event.key === "Enter") {
+      this.setState({sortBy: 'popular'});
+      this.props.onSearch(this.state.value, 'popular')
+    }
+  };
 
-    onSuggestionsFetchRequested = ({ value }) => {
-
-      fetch(config.suggester_url + "/" + value + "?cat=112")
-          .then(res => res.json())
-          .then(
-              (result) => {
-                  this.setState({
-                      suggestions: adaptSuggestions(result)
-                  });
-                },
-                (error) => {
-                  console.log('error')
-              }
-          )
+  onSuggestionsFetchRequested = ({value}) => {
+    fetch(`${config.suggester_url}/${value}?cat=112`).then(res => res.json()).then(result => {
+      this.setState({suggestions: adaptSuggestions(result)});
+    }, error => console.log(error))
   };
 
   // Autosuggest will call this function every time you need to clear suggestions.
-  onSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: []
-    });
-  };
+  onSuggestionsClearRequested = () => this.setState({suggestions: []});
 
   render() {
-    const { value, suggestions } = this.state;
+    const {value, suggestions} = this.state;
 
     // Autosuggest will pass through all these props to the input.
     const inputProps = {
@@ -101,29 +82,21 @@ class Search extends React.Component {
       onKeyPress: this.keyPressed
     };
 
-    return (
-      <div className="search">
-        <Autosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={inputProps}
-        />
-        <p className="sortby">
-          <span 
-            onClick={() => {this.changeSorting('date')}}
-            className={this.state.sortBy === 'date' ? 'active' : ''}
-            >recent
-          </span> | <span 
-          onClick={() => {this.changeSorting('popular')}}
-          className={this.state.sortBy !== 'date' ? 'active' : ''}
-          >popular
-          </span>
-        </p>
-      </div>
-    );
+    return (<div className="search">
+      <p className="sortby">
+        <span onClick={() => this.changeSorting('date')} className={this.state.sortBy === 'date'
+          ? 'active'
+          : ''}>recent
+        </span> |
+        <span onClick={() => {
+          this.changeSorting('popular')
+        }} className={this.state.sortBy !== 'date'
+          ? 'active'
+          : ''}> popular
+        </span>
+      </p>
+      <Autosuggest suggestions={suggestions} onSuggestionsFetchRequested={this.onSuggestionsFetchRequested} onSuggestionsClearRequested={this.onSuggestionsClearRequested} getSuggestionValue={getSuggestionValue} renderSuggestion={renderSuggestion} inputProps={inputProps} renderInputComponent={renderInput}/>
+    </div>);
   }
 }
 
