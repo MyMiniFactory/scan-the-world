fc55c68dd47f2f0d9cac3e2f43fb3c7c4ec3b6ec
pixtop <pixtopmc@gmail.com>
2019-06-26 14:48:42 +0100

    create a pure gatsby homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,37 @@
 import React from "react"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import './index.scss';
-import Objects from '../components/Objects/Objects';
-import Search from '../components/Search/Search';
+import Container from "../components/container"
+import './index.css';
 
 
-class ScanTheWorld extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = { showMenu: true };
-    this.state.query = '';
-    this.state.sortBy = 'date';
-    this.onSearch = this.onSearch.bind(this);
+const HomePage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <Container objects={data.allMyMiniFactoryObject.nodes}>
+      <div className="intro">
+        <h1>scan the world</h1>
+        <p>Scan The World is a movement archive objects of cultural significance using 3D scanning technologies, producing an extensive platform of content suitable for 3D printing.</p>
+      </div>
+    </Container>
+  </Layout>
+)
+
+export const query = graphql `
+  query MyQuery {
+    allMyMiniFactoryObject {
+      nodes {
+        images {
+          thumbnail {
+            url
+          }
+        }
+        name
+        url
+      }
+    }
   }
-  
-  onSearch(query, sortBy) {
-    console.log(sortBy)
-    this.setState({
-      query: query,
-      sortBy: sortBy
-    })
-  }
+`
 
-  render() {
-    return (
-      <Layout>
-        <SEO title="Home" />
-        <div className="content">
-          <Search
-            onSearch={this.onSearch}
-          />
-          <div className="scan-the-world">
-            <div className="intro">
-              <h1><b>scan</b> the <i>world</i></h1>
-              <p>Scan The World is a movement archive objects of cultural significance using 3D scanning technologies, producing an extensive platform of content suitable for 3D printing.</p>
-            </div>
-            <Objects
-              query={this.state.query}
-              sortBy={this.state.sortBy}
-            />
-          </div>
-        </div>
-      </Layout>
-    )
-  }
-}
-
-export default ScanTheWorld;
+export default HomePage;
