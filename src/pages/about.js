1a02a43c17a299c373eb39aac7d55880a89f3747
pixtop <pixtopmc@gmail.com>
2019-06-27 14:52:44 +0100

    integrate team to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,37 +2,27 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Team from "../components/team"
 import './about.css';
 
 
 const AboutPage = ({ data }) => (
   <Layout>
     <SEO title="About" />
-    <div class="about-container">
-      <div></div>
+    <div className="about-container">
       <div className="about-content" dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}/>
       <div className="team">
         <h2>the team</h2>
+        <Team />
       </div>
     </div>
-    </Layout>
+  </Layout>
     )
 
 export const query = graphql `
   query AboutQuery {
     markdownRemark(frontmatter: {path: {eq: "about"}}) {
       html
-    }
-    allMyMiniFactoryObject {
-      nodes {
-        images {
-          thumbnail {
-            url
-          }
-        }
-        name
-        url
-      }
     }
   }
 `
