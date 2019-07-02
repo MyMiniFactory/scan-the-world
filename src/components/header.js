7020677f9fb45597a42b78378b8ee2d7641673f1
pixtop <pixtopmc@gmail.com>
2019-07-02 15:22:16 +0100

    rework on header design

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,53 @@
 import React from "react"
 import { Link } from "gatsby"
-import "./header.css"
+import "./header.scss"
+import logo from "./stw_logo.svg"
 
-const MMY_URL = `https://www.myminifactory.com`
+class Header extends React.Component {
 
-export default () => (
-  <header>
-    <div>
-      <a href={MMY_URL}><img src={`logo.png`} alt="Logo"/></a>
-      <Link to={`/`}>
-        <img src={`stw-black.png`} alt="Scan the World" />
-      </Link>
-    </div>
-    <nav>
-      <Link to='/about'>About</Link>
-      <Link to='/contribute'>Contribute</Link>
-      <Link to='/learn' >Learn</Link>
-      <Link to='/community'>Community</Link>
-      <Link to='/museums'>Museums</Link>
-    </nav>
-  </header>
-)
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: null,
+      scroll: window.scrollY,
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    const scroll = window.scrollY;
+    if (scroll) {
+      scroll > this.state.scroll ? this.setState({status: `collapse`}) : this.setState({status: `open`});
+      this.setState({scroll: scroll});
+    }
+  }
+
+  render() {
+    return (
+      <header className={this.state.status}>
+        <div>
+          <span>
+            <img src={logo} alt="Scan the world" height='30'/>
+          </span>
+        </div>
+        <nav>
+          <Link to='/about'>About</Link>
+          <Link to='/contribute'>Contribute</Link>
+          <Link to='/learn' >Learn</Link>
+          <Link to='/community'>Community</Link>
+          <Link to='/museums'>Museums</Link>
+        </nav>
+      </header>
+  )
+  }
+}
+
+export default Header
