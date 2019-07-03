8f4161aa7ad816291469a8b1f14694812dad8512
pixtop <pixtopmc@gmail.com>
2019-07-03 15:54:00 +0100

    refactor 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
 import React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div style={{maxWidth:`1000px`, margin:`0 auto`}}>
+      <h1>404: NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>Click <Link to={'/'}>here</Link> to return to the home page.</p>
+    </div>
   </Layout>
 )
 
