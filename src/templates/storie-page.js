05bb10c03138ff59e1cdcc54d4edee522b27ac33
pixtop <pixtopmc@gmail.com>
2019-07-05 12:54:57 +0100

    add storie feature

---
+++ src/templates/storie-page.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+const StoriePage = () => {
+
+  return (
+    <Layout>
+
+    </Layout>
+  )
+}
+
+export default StoriePage
