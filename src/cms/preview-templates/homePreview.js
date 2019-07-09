1dde362874295e7a0dd36d37670496e2fac85be6
pixtop <pixtopmc@gmail.com>
2019-07-09 15:46:43 +0100

    disable rendering for now

--- src/cms/preview-templates/homePreview.js
+++ src/cms/preview-templates/homePreview.js
@@ -1,14 +1,8 @@
 import React from 'react'
-import Gallery from 'react-photo-gallery'
 
 const HomePreview = ({ entry }) => {
-  const data = entry.getIn(['data']).toJS()
-
-  if (data) {
-    return <Gallery photos={data.trends} direction={`column`}/>
-  } else {
-    return <div>Loading...</div>
-  }
+  // const data = entry.getIn(['data']).toJS()
+  return <div>Loading...</div>
 }
 
 export default HomePreview;
