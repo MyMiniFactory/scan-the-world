ff7a1339e19b60cde8a9931a9c488b84fa1aab58
pixtop <pixtopmc@gmail.com>
2019-07-12 17:38:14 +0100

    rework banner component

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,15 +1,8 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import { FaChevronLeft } from 'react-icons/fa'
 import './banner.css'
 
 const Banner = (props) => (
-  <>
-    <Link to={props.url}>
-      <FaChevronLeft className='return'/>
-    </Link>
     <div className="banner" style={{backgroundImage: `url(${props.bannerUrl})`}}/>
-  </>
 )
 
 export default Banner
