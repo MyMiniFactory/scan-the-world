d43b2667867ea8b112a0ecfb858371834712189f
pixtop <pixtopmc@gmail.com>
2019-07-16 16:53:43 +0100

    add designer tile

--- src/components/ObjectTile/ObjectTile.js
+++ src/components/ObjectTile/ObjectTile.js
@@ -3,12 +3,18 @@ import './ObjectTile.css'
 import React from 'react';
 
 function ObjectTile({ object }) {
+  const { designer } = object
   return (
     <div className="object-tile">
       <a href={object.url} >
         <img src={object.images[0].thumbnail.url} alt={object.name}/>
       </a>
       <p>{object.name}</p>
+      {designer &&
+        <a className="object-designer" href={designer.profile_url}>
+          <img src={designer.avatar_thumbnail_url} alt={designer.name}/>
+        </a>
+      }
     </div>
   );
 }
