53ed0ccbdbd900770926947012b36a1ef64e6be3
pixtop <pixtopmc@gmail.com>
2019-08-05 17:59:20 +0100

    integrate random component to the container

--- src/components/searchContainer.js
+++ src/components/searchContainer.js
@@ -1,37 +1,51 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Objects from './Objects/Objects'
 import Search from './Search/Search'
 import Header from './header'
+import RandomArt from './randomArt'
 import './searchContainer.css'
 
 class SearchContainer extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       objects: props.objects,
       query: ``,
       sortBy: `popularity`,
-    };
-    this.onSearch = this.onSearch.bind(this);
+      showingRandom: false,
+    }
+    this.onSearch = this.onSearch.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
   onSearch(value, sortBy) {
     this.setState({
+      objects: [],
       query: value,
       sortBy: sortBy,
+      showingRandom: sortBy === 'random',
     });
   }
 
+  handleClick() {
+    this.setState({showingRandom: this.state.showingRandom})
+  }
+
   render() {
+    const content = this.state.showingRandom
+      ? <RandomArt />
+      : <Objects objects={this.state.objects} query={this.state.query} sortBy={this.state.sortBy} url={this.props.url}/>
     return (
       <div className="search-container">
         <div className="search-wrap">
           <Header isHome={true} />
           <Search sortBy={this.state.sortBy} onSearch={this.onSearch} />
+          <Link to='/map'>map</Link>
         </div>
-        <Objects objects={this.state.objects} query={this.state.query} sortBy={this.state.sortBy} url={this.props.url}/>
+        {content}
       </div>
-    );
+    )
   }
 }
 
