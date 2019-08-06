605baf319d198f84f9838f6a68d8fb873836bfcf
pixtop <pixtopmc@gmail.com>
2019-08-06 16:38:45 +0100

    rework on style

--- src/components/ObjectTile/ObjectTile.js
+++ src/components/ObjectTile/ObjectTile.js
@@ -9,7 +9,11 @@ function ObjectTile({ object }) {
       <a href={threedObject.url} >
         <img src={threedObject.images[0].thumbnail.url} alt={object.title}/>
       </a>
-      <p>{`${object.title}${object.place ? ', ' + object.place : ''} ${object.artist ? 'by ' + object.artist : ''}`}</p>
+      <p>
+        <span style={{fontWeight:`bold`}}>{object.title}</span>
+        {object.place ? <span>,  {object.place}</span> : ''}
+        {object.artist ? <span style={{fontStyle:`italic`}}>{' by ' + object.artist}</span> : ''}
+      </p>
       {threedObject.designer &&
         <a className="object-designer" href={threedObject.designer.profile_url}>
           <img src={threedObject.designer.avatar_thumbnail_url} alt={threedObject.designer.name}/>
