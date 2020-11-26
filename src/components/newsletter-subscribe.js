623cfcf48740db9be2e206029c5fd568dd755756
Felix Li <felix@myminifactory.com>
2020-11-26 13:35:53 +0000

    Connect the form to STW mailchimp subcription action

--- src/components/newsletter-subscribe.js
+++ src/components/newsletter-subscribe.js
@@ -15,25 +15,31 @@ class NewsletterSubscribe extends React.Component {
 
 	render() {
 		return (
-		<form className="newletterSubscribe">
+		<form 
+			className="newletterSubscribe"
+			action="https://myminifactory.us11.list-manage.com/subscribe/post?u=d472931d3b83139f3fe4179d3&id=71e6a4bafd"
+			method="post" 
+			id="mc-embedded-subscribe-form" 
+			name="mc-embedded-subscribe-form" 
+			target="_blank"
+		>
 			<label 
 				className="newletterSubscribeLabel"
-				htmlFor="emailAddress"
+				htmlFor="mce-EMAIL"
 			>
 				Subscribe to our Newsletter
 			</label>
 			<div className="newletterFlex">
 				<input 
-					id="emailAddress"
+					id="mce-EMAIL"
 					className="newletterInput" 
 					type="text" 
-					name="emailAddress"
+					name="EMAIL"
 					placeholder="Email Address"
 					value={this.state.email}
 					onChange={this.handleChange}
 				/>
-				{/* <input type="submit" value="Submit" /> */}
-				<button className="submitButton">subscribe</button>
+				<input className="submitButton" type="submit" value="subscribe" />
 			</div>
 		</form>
 		)
